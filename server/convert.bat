1de4fbaa35f4daa051eda23b46afc08e84f6d5b3
David Roth <derpydeponson@gmail.com>
2019-12-04 17:52:29 +1100

    woops haha, oopsie haha, little WOOPSY hahah OOPSIE haha :^)

--- server/convert.bat
+++ server/convert.bat
@@ -1,1 +1,1 @@
-ffmpeg -y -i "NGHTHYP #Champloorecords Out of air mix.mp3"  -acodec pcm_s16le -f s16le -ac 1 -ar 16000 out.raw
+ffmpeg -y -i "NGHTHYP #Champloorecords Out of air mix.mp3"  -acodec pcm_s16le -f s16le  out.raw
